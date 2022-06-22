5dafba1a41ffc8aac7b25e8296e25f823f886ffa
TorsteinAndvik <torstein.andvik@gmail.com>
2022-06-22 17:12:05 +0200

    Imported sample code

---
+++ trello.py
@@ -0,0 +1,9 @@
+import requests
+key = "your_key"
+token = "your_token"
+def create_board(board_name):
+    url = "https://api.trello.com/1/boards/"
+    querystring = {"name": board_name, "key": key, "token": token}
+    response = requests.request("POST", url, params=querystring)
+    board_id = response.json()["shortUrl"].split("/")[-1].strip()
+    return board_id
